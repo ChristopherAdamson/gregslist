e6d5f3ae90e77a3ed6b58226f211db7a2ba07000
christopher adamson <christopheradamson98@gmail.com>
2020-07-08 13:48:02 -0600

    init

---
+++ app/Controllers/CarController.js
@@ -0,0 +1,11 @@
+import SERVICE from "../Services/CarService.js"
+import _store from "../store.js"
+
+
+export default class CarController {
+  constructor() {
+    console.log("hello from car controller");
+
+
+  }
+}
---
+++ app/Services/CarService.js
@@ -0,0 +1,14 @@
+import _store from "../store.js"
+
+class CarsService {
+  constructor() {
+    console.log("hello from car service")
+  }
+}
+
+
+const SERVICE = new CarsService()
+
+export default SERVICE
+
+
